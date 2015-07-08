358066f4f7769048230b0f54817ebef073244faf
michaelanfield <michael.anfield@googlemail.com>
2015-07-08 16:21:26 +0100

    A little doc on the sweetness of the and operator

--- exercise-3/exercise-3.rb
+++ exercise-3/exercise-3.rb
@@ -3,14 +3,12 @@
 # the sum of the previous two. As an example, here are the first 10 Fibonnaci numbers: 
 # 0, 1, 1, 2, 3, 5, 8, 13, 21, and 34.
 
-def fibonacci(max_to)
+def fibonacci(sequence_up_to)
 	fibonacci_sequence = [0, 1]
 
-	index = 2
-	while index < max_to
-		next_numbers = fibonacci_sequence.last(2)
-
-		fibonacci_sequence << next_numbers.first + next_numbers.last
+	index = 0
+	while index < (sequence_up_to - 2)
+		fibonacci_sequence << fibonacci_sequence.last(2).reduce(0, :+)
 		index += 1
 	end
 
