b08ece140a3a438ac0d574315141d705ff719f78
michaelanfield <michael.anfield@googlemail.com>
2015-07-09 15:11:09 +0100

    Removed an errant space as it was messing with my white space mojo.

--- exercise-4/clever_collections.rb
+++ exercise-4/clever_collections.rb
@@ -15,7 +15,7 @@ shopping_list = [:orange, :apple, :apple, :cereal, :bread]
 #TODO - print out in pounds and pence the total for the shopping list
 # Msg:: The price of the shopping list is: £pounds.pence
 
-total = shopping_list.map { |item| prices[item.to_s]  }.reduce(:+)
+total = shopping_list.map { |item| prices[item.to_s] }.reduce(:+)
 
 # binding.pry
 p "The price of the shopping list is: £#{sprintf('%03d', total).insert(-3, '.')}"
