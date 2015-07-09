0b123420208943805edabfcf0462f1429d22bfac
michaelanfield <michael.anfield@googlemail.com>
2015-07-09 15:07:25 +0100

    Added a string formatting example and installed the money gem to try out using the money class. Thats much shine. I like it.

--- exercise-4/clever_collections.rb
+++ exercise-4/clever_collections.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'money'
 
 prices = {
       'orange' => 10,
@@ -15,5 +16,13 @@ shopping_list = [:orange, :apple, :apple, :cereal, :bread]
 # Msg:: The price of the shopping list is: £pounds.pence
 
 total = shopping_list.map { |item| prices[item.to_s]  }.reduce(:+)
+
 # binding.pry
 p "The price of the shopping list is: £#{sprintf('%03d', total).insert(-3, '.')}"
+
+# Example using the string formatter, converting the total to a float so
+# we can convert it to pounds and pence.
+p "The price of the shopping list is: £#{'%.2f' % (total.to_f / 100)}"
+
+# Example using the money gem
+p "The price of the shopping list is: #{Money.new(total, "GBP").format}"
