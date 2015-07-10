52697626349510e16f78daac3b3d2eb51a59f027
michaelanfield <michael.anfield@googlemail.com>
2015-07-10 13:35:24 +0100

    First pass at the 2nd stringnasium exercise. Got it working through a series of gsub's and converting strings to numbers. Its smelly though!

--- exercise-6/string_gymnastics_2.rb
+++ exercise-6/string_gymnastics_2.rb
@@ -1,3 +1,6 @@
+require 'pry'
+require 'money'
+
 price_list = "orange = 10p apple = 20p bread = £1.10 tomato = 25p cereal = £2.34" 
 
 shopping_list=<<LIST
@@ -17,3 +20,8 @@ LIST
 Given the following price list and shopping list print out the total cost of 
 the shopping list in pounds and pence
 =end 
+
+price_list = price_list.gsub(' = ', '=').gsub('p','').gsub('£','').gsub('.', '').split.map { |item| item.split('=') }.to_h
+total = shopping_list.split.map { |item| price_list[item].to_i || 0 }.reduce(:+)
+
+p "The price of the shopping list is: #{Money.new(total, 'GBP').format}"
