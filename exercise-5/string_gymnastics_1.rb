967f80ee854d3eb7d223b9c14d3f4cdb337c50a7
michaelanfield <michael.anfield@googlemail.com>
2015-07-10 13:07:01 +0100

    Completed the stringnasium with a few solutions using map, reject and collect. The first solution to deriving the total is the simplest / sweetest IMO.

--- exercise-5/string_gymnastics_1.rb
+++ exercise-5/string_gymnastics_1.rb
@@ -1,3 +1,6 @@
+require 'pry'
+require 'money'
+
 prices = {
       'orange' => 10,
       'apple' => 20,
@@ -22,4 +25,21 @@ LIST
 =begin
 Given the following price list and shopping list print out the total cost of 
 the shopping list in pounds and pence
+
+The price of the shopping list is: £pounds.pence
 =end 
+
+# Items that are not in the prices hash are evaluated as 0 using .map
+total = shopping_list.split.map { |item| prices[item] || 0 }.reduce(:+)
+
+p "The price of the shopping list is: £#{'%.2f' % (total.to_f / 100)}"
+
+# Firstly removes items from the shopping list that are not in the prices hash and then derives the total
+total = shopping_list.split.reject { |item| !prices.key?(item) }.map { |item| prices[item] }.reduce(:+)
+
+p "The price of the shopping list is: #{Money.new(total, 'GBP').format}"
+
+# Items that are not in the prices hash are evaluated as 0 using .collect
+total = shopping_list.split.collect { |item| prices.key?(item) ? prices[item] : 0 }.reduce(:+)
+
+p "The price of the shopping list is: #{Money.new(total, 'GBP').format}"
