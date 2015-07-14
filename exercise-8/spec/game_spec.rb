785400a2ce8b40b0cd9331266f2b10c0a5005722
Michael Anfield <michael.anfield@sky.uk>
2015-07-14 13:44:45 +0100

    Added in a named `let`, which sets a description variable based on the description of each example. This allows us to use the description within each of the test evaluations.

--- exercise-8/spec/game_spec.rb
+++ exercise-8/spec/game_spec.rb
@@ -1,27 +1,32 @@
 require 'spec_helper'
 
 describe Game do
+
+	let(:description) do |example|
+		example.description
+	end
+
 	describe '#evaluate' do
 
 		subject(:game) { Game.new }
 
 		context 'when the choices are different' do
 			it 'paper beats rock' do
-				expect(game.evaluate('paper', 'rock')).to eq('paper beats rock')
+				expect(game.evaluate('paper', 'rock')).to eq(description)
 			end
 
 			it 'scissors beats paper' do
-				expect(game.evaluate('scissors', 'paper')).to eq('scissors beats paper')
+				expect(game.evaluate('scissors', 'paper')).to eq(description)
 			end
 
 			it 'rock beats scissors' do
-				expect(game.evaluate('rock', 'scissors')).to eq('rock beats scissors')
+				expect(game.evaluate('rock', 'scissors')).to eq(description)
 			end
 		end
 
 		context 'when the choices are the same' do
 			it "it's a draw" do
-				expect(game.evaluate('rock', 'rock')).to eq("it's a draw")
+				expect(game.evaluate('rock', 'rock')).to eq(description)
 			end
 		end
 	end
