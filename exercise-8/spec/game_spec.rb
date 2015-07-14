3c795512feaa803808b70772466534f257a09f36
Michael Anfield <michael.anfield@sky.uk>
2015-07-14 12:40:28 +0100

    Refactored to create a named subject as this is the preferred way of doing it according to the spec docs. I guess its less confusing / magical.

--- exercise-8/spec/game_spec.rb
+++ exercise-8/spec/game_spec.rb
@@ -2,23 +2,26 @@ require 'spec_helper'
 
 describe Game do
 	describe '#evaluate' do
+
+		subject(:game) { Game.new }
+
 		context 'when the choices are different' do
 			it 'paper beats rock' do
-				expect(subject.evaluate('paper', 'rock')).to eq('paper beats rock')
+				expect(game.evaluate('paper', 'rock')).to eq('paper beats rock')
 			end
 
 			it 'scissors beats paper' do
-				expect(subject.evaluate('scissors', 'paper')).to eq('scissors beats paper')
+				expect(game.evaluate('scissors', 'paper')).to eq('scissors beats paper')
 			end
 
 			it 'rock beats scissors' do
-				expect(subject.evaluate('rock', 'scissors')).to eq('rock beats scissors')
+				expect(game.evaluate('rock', 'scissors')).to eq('rock beats scissors')
 			end
 		end
 
 		context 'when the choices are the same' do
 			it "it's a draw" do
-				expect(subject.evaluate('rock', 'rock')).to eq("it's a draw")
+				expect(game.evaluate('rock', 'rock')).to eq("it's a draw")
 			end
 		end
 	end
