e63ee0e8fa8d5ef150e558ac9cdc9b3cdc10efdd
Michael Anfield <michael.anfield@sky.uk>
2015-07-20 16:32:18 +0100

    Tweeted the time tests so they are bit more complete, checking for an expected amount of elapsed time. Still no TimeCop integration.

--- exercise-14/spec/wait_spec.rb
+++ exercise-14/spec/wait_spec.rb
@@ -41,16 +41,34 @@ describe Wait do
 			expect{ Wait.until }.to raise_error(Wait::NoBlockGivenError)
 		end
 
-		it 'will wait 1 second between block executions' do
-			expect(Wait.until(:retry_time => 1) { rand(999) % 2 == 0 }).to be(true)
+		it 'will wait 0.01 second between block executions' do
+			expect(Wait.until(:retry_time => 0.01) { rand(999) % 2 == 0 }).to be(true)
 		end
 
 		it 'will expire after 5 seconds by default' do
-			expect{ Wait.until { false } }.to raise_error(Wait::TimeOutError)
+			start_time = Time.now
+
+			begin
+				Wait.until { false }
+				fail 'Expected a Wait::TimeOutError'
+			rescue Wait::TimeOutError => error
+				expect(error.to_s).to include('Timed out waiting, 5 seconds elapsed')
+			end
+
+			expect(Time.now - start_time).to be >= 5
 		end
 
-		it 'will expire after 3 seconds' do
-			expect{ Wait.until(:expire_after => 3) { false } }.to raise_error(Wait::TimeOutError)
+		it 'will expire after 0.01 seconds' do
+			start_time = Time.now
+
+			begin
+				Wait.until(:expire_after => 0.01) { false }
+				fail 'Expected a Wait::TimeOutError'
+			rescue Wait::TimeOutError => error
+				expect(error.to_s).to include('Timed out waiting, 0.01 seconds elapsed')
+			end
+
+			expect(Time.now - start_time).to be >= 0.01
 		end
 	end
 end
