7c51de08dbbdb714f0fc5cc11fb6625c8ca77603
Michael Anfield <michael.anfield@sky.uk>
2015-07-20 16:22:24 +0100

    Added in the expire_after parameter which defaults to 5 seconds. Created a TimeOutError for this. Tests still require a little more finesse. Don't want to have to wait at least 8 seconds for them to run.

--- exercise-14/spec/wait_spec.rb
+++ exercise-14/spec/wait_spec.rb
@@ -6,10 +6,17 @@ module Wait
 		raise NoBlockGivenError, 'You muppet! Please supply a block to execute.' unless block_given? 
 
 		retry_time = options.delete(:retry_time) || 0
-		
+		expire_after = options.delete(:expire_after) || 5
+
+		start_time = Time.now
+
 		outcome = yield
 
 		while !outcome
+			current_time = Time.now
+
+			raise TimeOutError, "Timed out waiting, #{expire_after} seconds elapsed" if current_time - start_time >= expire_after
+
 			sleep(retry_time)
 
 			outcome = yield
@@ -19,12 +26,11 @@ module Wait
 	end
 
 	class NoBlockGivenError < StandardError; end
+
+	class TimeOutError < StandardError; end
 end
 
 describe Wait do
-	# Define a module called Wait 
-	# With an until method 
-	# method should take a block which will execute until it returns true
 
 	describe '#until' do
 		it 'will execute block until it returns true' do
@@ -37,6 +43,14 @@ describe Wait do
 
 		it 'will wait 1 second between block executions' do
 			expect(Wait.until(:retry_time => 1) { rand(999) % 2 == 0 }).to be(true)
+		end
+
+		it 'will expire after 5 seconds by default' do
+			expect{ Wait.until { false } }.to raise_error(Wait::TimeOutError)
+		end
+
+		it 'will expire after 3 seconds' do
+			expect{ Wait.until(:expire_after => 3) { false } }.to raise_error(Wait::TimeOutError)
 		end
 	end
 end
