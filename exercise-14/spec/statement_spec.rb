2b2153bcecfd539432145ebcf2b6b5c2cfa6cb4b
Michael Anfield <michael.anfield@sky.uk>
2015-08-03 14:32:35 +0100

    Added in a module to take on some of the strain for the common functionality and got it working so it does everything but provide a statement total. This is next on the agenda. Still requires refactoring as there are areas that are screaming out. Also need to get them broken down into their own files.

--- exercise-14/spec/statement_spec.rb
+++ exercise-14/spec/statement_spec.rb
@@ -1,31 +1,101 @@
 require 'pry'
 require 'json'
 
-class Statement
+module DSLBuilder
 
-	METHOD_NAME_OVERRIDES = {
-		:date => :generated,
-		:from => [:period, :from]
-	}
-
-	attr_reader :body
-
-	def self.build(debug = false, &block)
-		statement = Statement.new(debug)
-		statement.instance_eval(&block) unless !block_given?
-		statement
-	end
-
-	def initialize(debug = false)
-		@body = {}
-		@debug = debug
-	end
+	attr_reader :method_name_overrides, 
+							:method_data_types,
+							:body
 
 	def method_missing(method, *args, &block)
 		debug_method_call(method, *args)
 
-		body[METHOD_NAME_OVERRIDES[method] || method] = block_given? ? instance_eval(&block) : args.first
-  end
+		method_name = clean_method_name(method)
+
+		if block_given?
+			if !args.empty?
+				method_value = Call.build(args, &block).body
+			else
+				method_value = Statement.build(@debug, *args, &block).body
+			end
+			#instance_eval(&block)
+
+			p "Method #{method_name}, with block has a value of: #{method_value}"
+
+			register_method_value(method_name, method_value)
+		else
+			register_method_value(method_name, args.first)
+		end
+	end
+
+	private 
+	def clean_method_name(method_name)
+		debug_method_call('clean_method_name', method_name)
+
+		method_name.to_s.camel_case_lower.to_sym
+	end
+
+	def register_method_value(method_name, method_value)
+		debug_method_call('register_method_value', method_name, method_value)
+
+		method_override = method_name_overrides[method_name]
+
+		if method_override && ( method_override.is_a? Array )	
+
+			method_override.each_with_index do |override, index|
+				body[override] = body[override] || {}
+
+				body[override][method_name] = method_value
+			end
+		else
+			method_data_type = method_data_types[method_name]
+
+p "Method data type for '#{method_name}' is: #{method_data_type}"
+
+			if method_data_type && method_data_type.is_a?(Array)
+				p 'We have an array'
+				body[method_override || method_name] = body[method_override || method_name] || method_data_type
+
+				body[method_override || method_name] << method_value 
+			else
+				body[method_override || method_name] = method_value
+			end
+		end
+	end
+
+	def debug_method_call(method, *args)
+		if @debug
+			p "Calling method #{method}"
+			args.each_with_index do |arg, index|
+				p "#{index} - #{arg}:#{arg.class}"
+			end
+		end
+	end
+end
+
+class Statement
+
+	include DSLBuilder
+
+	def self.build(debug = false, *args, &block)
+		statement = Statement.new(debug)
+		statement.instance_eval(&block) if block_given?
+		statement
+	end
+
+	def initialize(debug)
+		@body = {}
+		@debug = debug
+		@method_name_overrides = {
+			:date => :generated,
+			:from => [:period],
+			:to => [:period],
+			:call => :calls
+		}
+		@method_data_types = {
+			:call => []
+		}
+	end
 
   def to_json
   	statement = {
@@ -35,6 +105,8 @@ class Statement
 		statement[:statement].tap do |h|
 			h[:generated] = @date || Date.today
 		end
+
+		p statement.to_json if @debug
 
   	statement.to_json
   end
@@ -49,25 +121,37 @@ class Statement
 
 		@date
 	end
-
-	def resolve_method_location()
-	end
-
-	def debug_method_call(method, *args)
-		if @debug
-			p "Calling method #{method}"
-			args.each_with_index do |arg, index|
-				p "#{index} - #{arg}:#{arg.class}"
-			end
-		end
-	end
 end
+
+class Call
+	include DSLBuilder
+
+	def self.build(*args, &block)
+		call = Call.new(args.flatten.first)
+		call.instance_eval(&block) if block_given?
+		call
+	end
+
+	def initialize(phone_number)
+		@body = {}
+		@method_name_overrides = {}
+		@method_data_types = {}
+
+		body[:called] = phone_number
+	end
+end	
 
 class Fixnum
 	def days
 		self
 	end
 end		
+
+class String
+  def camel_case_lower
+    self.split('_').inject([]){ |buffer,e| buffer.push(buffer.empty? ? e : e.capitalize) }.join
+  end
+end
 
 describe Statement do
 
@@ -115,7 +199,7 @@ describe Statement do
 			expected_from_date = Date.parse('2015-01-01')
 			expected_to_date = Date.parse('2015-01-31')
 
-			statement = Statement.build(true) do
+			statement = Statement.build do
 				from Date.parse('2015-01-01')
 				to Date.parse('2015-01-31')
 			end
@@ -125,6 +209,32 @@ describe Statement do
 
 			expect(from_date).to eq(expected_from_date.to_s)
 			expect(to_date).to eq(expected_to_date.to_s)
+		end
+
+		it 'will have call charges that contain calls' do
+			expected_call_1 = { "called" => "07716393769", "date" => "2015-01-26", "duration" => "00:23:03", "cost" => 1.13 }
+			expected_call_2 = { "called" => "07719999999", "date" => "2015-01-28", "duration" => "00:12:23", "cost" => 0.20 }
+
+			statement = Statement.build(true) do
+				call_charges do
+					call '07716393769' do
+						date Date.parse('2015-01-26')
+			      duration "00:23:03"
+			      cost 1.13
+			    end
+			    call '07719999999' do
+						date Date.parse('2015-01-28')
+			      duration "00:12:23"
+			      cost 0.20
+			    end
+				end
+			end
+
+			call_1 = JSON.parse(statement.to_json)['statement']['callCharges']['calls'].first
+			call_2 = JSON.parse(statement.to_json)['statement']['callCharges']['calls'].last
+
+			expect(call_1).to eq(expected_call_1)
+			expect(call_2).to eq(expected_call_2)
 		end
 	end
 end
